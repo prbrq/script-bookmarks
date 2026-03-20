51b7e18dc1cb7aec4b2fa0df76c99fbff5e18ad9
Arkady Chelyadnikov <prbrq@ya.ru>
2026-03-20 19:18:56 +0300

    swagger

---
+++ src/lib/swagger.ts
@@ -0,0 +1,9 @@
+export const swaggerSpec = {
+  openapi: "3.0.0",
+  info: {
+    title: "Bookmarks API",
+    version: "1.0.0",
+    description: "API for managing bookmarks with tags",
+  },
+  paths: {},
+};
